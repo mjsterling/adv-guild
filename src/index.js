be2894f2b8a2f639c523428b4ceff3eeaac18bb3
Matthew Sterling <mjsterling93@gmail.com>
2021-03-14 19:26:42 +1100

    working components

--- src/index.js
+++ src/index.js
@@ -9,16 +9,15 @@ const items = [
   ["helmet0", "No Helmet", 0, 1, 1],
   ["body0", "No Armour", 0, 1, 1],
   ["legs0", "No Legs", 0, 1, 1],
-  ["weapon1", "Aluminium Sword", 5, 1, 0.9]
-  ["gloves1", "Foil Gloves", 2, 0.95, 0.95],
+  ["weapon1", "Aluminium Sword", 5, 1, 0.9][
+    ("gloves1", "Foil Gloves", 2, 0.95, 0.95)
+  ],
   ["helmet1", "Foil Helmet", 1, 0.95, 0.95],
   ["body1", "Foil Chestplate", 1, 0.95, 0.95],
   ["legs1", "Foil Platelegs", 1, 0.95, 0.95],
-]
+];
 //0 set id, 1 attack boost (additive), 2 dmgreduction boost (multiplicative), 3 aspd boost (multiplicative)
-const setbonuses = [
-  ["set1", "Full Aluminium", 10, 0.9, 0.9]
-]
+const setbonuses = [["set1", "Full Aluminium", 10, 0.9, 0.9]];
 //const arr
 //list of items and their stat bonuses
 //const arr
@@ -32,40 +31,40 @@ class Game extends React.Component {
     this.state = {
       // hero name, current hp, max hp, weapon, gloves, helmet, body, legs, current enemy selection, monster current hp, monster max hp
       heroes: [
-          ["Warrior", 100, 100, "weapon0", "gloves0", "helmet0", "body0", "legs0", null, 30, 30]
+        [
+          "Warrior",
+          100,
+          100,
+          "weapon0",
+          "gloves0",
+          "helmet0",
+          "body0",
+          "legs0",
+          "Aluminium Golem",
+          30,
+          30,
+        ],
       ],
       //resource name, resource amount
       inventory: [
         ["Aluminium", 0],
         ["Balsa Wood", 0],
-        ["Hessian Cloth", 0]
-      ]
-      }
+        ["Hessian Cloth", 0],
+      ],
     };
-
-  render() {
-    return (
-      <div>
-        <Container />
-        <Menubar />
-      </div>
-    );
-  }
-}
-
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
   }
 
   render() {
     return (
       <div>
-        <Fight className="hero1" />
-        {/* <Fight className={hero2} />
+        <div className="container">
+          <Fight classname="hero1" hero={this.state.heroes[0]} />
+          {/* <Fight className={hero2} />
         <Fight className={hero3} />
         <Fight className={hero4} />
         <Fight className={hero5} /> */}
+          <Menubar />
+        </div>
       </div>
     );
   }
@@ -73,52 +72,52 @@ class Container extends React.Component {
 
 class Fight extends React.Component {
   render() {
-    const hero1 = this.props.heroes;
-    const hero1hppc = Math.trunc(hero1[1] / hero1[2] * 100);
+    const hero = this.props.hero;
+    const herohppc = Math.trunc((hero[1] / hero[2]) * 100);
+    const monsterhppc = Math.trunc((hero[9] / hero[10]) * 100);
     return (
       <div>
-        <div className="heroname">{hero1[0]}</div>
-            <div className="hpcontainer">
-              <div className="herohpbar" width={hero1hppc + "%"}></div>
-              <div className="herohp">{hero1[1] + " / " + hero1[2]}</div>
-              </div>
-              <button className="herobtn">{hero1[0]}</button>
+        <div className="heroname">{hero[0]}</div>
+        <div className="hpcontainer">
+          <div className="herohpbar" width={herohppc + "%"}></div>
+          <div className="herohp">{hero[1] + " / " + hero[2]}</div>
+        </div>
+        <button className="herobtn">{hero[0]}</button>
 
-              <div className="monster">
-        <div className="monstername">{hero1[6]}</div>
-            <div className="hpcontainer">
-              <div className="monsterhpbar" width={hero1hppc + "%"}></div>
-              <div className="monsterhp">{hero1[7] + " / " + hero1[8]}</div>
-              </div>
-              <button className="monsterbtn">{hero1[6]}</button>
+        <div className="monster">
+          <div className="monstername">{hero[8]}</div>
+          <div className="hpcontainer">
+            <div className="monsterhpbar" width={monsterhppc + "%"}></div>
+            <div className="monsterhp">{hero[9] + " / " + hero[10]}</div>
+          </div>
+          <button className="monsterbtn">{hero[8]}</button>
         </div>
-        </div>
-    )
-}
+      </div>
+    );
+  }
 }
 
 class Menubar extends React.Component {
-
   render() {
     return (
       <div>
-<button className="settingsbtn" onClick={this.settings()}>Settings</button>
-<button className="inventorybtn" onClick={this.inventory()}>Settings</button>
+        {/* <button className="settingsbtn" onClick={this.settings()}>Settings</button>
+<button className="inventorybtn" onClick={this.inventory()}>Settings</button> */}
       </div>
-    )
+    );
   }
 }
 
 class Settings extends React.Component {
   render() {
-    return(
+    return (
       <div>
         {/* Settings menu
               - Sound
               - Music
                */}
       </div>
-    )
+    );
   }
 }
 
@@ -130,7 +129,7 @@ class Inventory extends React.Component {
             -list of obtained resources
             -amounts of each */}
       </div>
-    )
+    );
   }
 }
 ReactDOM.render(<Game />, document.getElementById("root"));
