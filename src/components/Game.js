cb0070455107e8b1be893b1a788cb58364ce41d7
Matthew Sterling <mjsterling93@gmail.com>
2021-03-21 16:00:17 +1100

    halved droprates

--- src/components/Game.js
+++ src/components/Game.js
@@ -313,14 +313,12 @@ class Game extends React.Component {
     let inventory = this.state.inventory;
     let log = this.state.log;
     let roll = monster.tier >= 5 && monster.tier < 9 ? Math.floor(Math.random() * 100) : "";
-    let rollMsg = roll !== "" ? " Roll: " + roll + ". " : "";
     if (monster.tier >= 5 && roll < 95) {
       log.push(
         monster.name +
           monsterDeathMsgs[
             Math.floor(Math.random() * monsterDeathMsgs.length)
           ] +
-          rollMsg +
           "Loot: Nothing!"
       );
     } else {
@@ -332,7 +330,6 @@ class Game extends React.Component {
     log.push(
       monster.name +
         monsterDeathMsgs[Math.floor(Math.random() * monsterDeathMsgs.length)] +
-        rollMsg +
         " Loot: " +
         dropamount +
         " " +
